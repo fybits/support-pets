3abb0426087f2a1ff198aa8539549925d3e33534
fybits <sergeig1313@gmail.com>
2020-04-14 16:35:33 +0300

    Phone input field mask and minor visual bugfixes

--- js/main.js
+++ js/main.js
@@ -12,25 +12,29 @@ function disableHiddenInputs() {
     });
 }
 
-$('form').on('submit', (event) => {
-  event.preventDefault();
-
-  let prev = $(`.tab[href="#${event.target.id}"]`);
-  prev.addClass('done');
-  prev.find('img').get(0).src = 'imgs/icon_complete.png';
+$(() => {
   
-  let tabs = $('.tabs');
-  tabs.removeClass('d-none');
-  tabs.addClass('d-flex');
-
-  if (event.target.nextElementSibling) {
-    console.log('changing')
-    let nextForm = $(event.target.nextElementSibling);
-    disableHiddenInputs();
-    let next = $(`.tab[href="#${nextForm.get(0).id}"]`);
-    next.removeClass('hidden');
-    next.tab('show');
-  }
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+  
+    let prev = $(`.tab[href="#${event.target.id}"]`);
+    prev.addClass('done');
+    prev.find('img').get(0).src = 'imgs/icon_complete.png';
+    
+    let tabs = $('.tabs');
+    tabs.removeClass('d-none');
+    tabs.addClass('d-flex');
+  
+    if (event.target.nextElementSibling) {
+      console.log('changing')
+      let nextForm = $(event.target.nextElementSibling);
+      disableHiddenInputs();
+      let next = $(`.tab[href="#${nextForm.get(0).id}"]`);
+      next.removeClass('hidden');
+      next.tab('show');
+    }
+  });
+  $('input[type=phone]').mask("999-999-9999")
 });
 
 window.addEventListener('resize', disableHiddenInputs)
