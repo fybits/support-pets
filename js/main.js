4d4b5c67229a8714ecdfb51d206ae6c5e45ad13c
fybits <sergeig1313@gmail.com>
2020-04-16 16:52:18 +0300

    Added mask for card number; Added possibility to switch to the completed tabs; Lightweight image;

--- js/main.js
+++ js/main.js
@@ -13,10 +13,25 @@ function disableHiddenInputs() {
 }
 
 $(() => {
-  
-  $('form').on('submit', (event) => {
+  $('.tab').on('click', (event) => {
     event.preventDefault();
-  
+    let newTab = $(event.currentTarget);
+    if (!newTab.hasClass('hidden')) {
+      newTab.addClass('active');
+      newTab.removeClass('done');
+      newTab.siblings().removeClass('active');
+      newTab.siblings().addClass('done');
+      let tabContent = $(`.tab-pane${event.currentTarget.getAttribute('href')}`);
+      tabContent.addClass('active');
+      tabContent.addClass('show');
+      tabContent.siblings().removeClass('active');
+      tabContent.siblings().removeClass('show');
+    }
+  });
+  $('.form').on('submit', (event) => {
+    event.preventDefault();
+    window.location.href = '#form';
+    
     let prev = $(`.tab[href="#${event.target.id}"]`);
     prev.addClass('done');
     prev.find('img').get(0).src = 'imgs/icon_complete.png';
@@ -26,15 +41,17 @@ $(() => {
     tabs.addClass('d-flex');
   
     if (event.target.nextElementSibling) {
-      console.log('changing')
       let nextForm = $(event.target.nextElementSibling);
       disableHiddenInputs();
       let next = $(`.tab[href="#${nextForm.get(0).id}"]`);
       next.removeClass('hidden');
+      next.removeClass('done');
       next.tab('show');
     }
+    window.scrollTo()
   });
-  $('input[type=phone]').mask("999-999-9999")
+  $('input[type=phone]').mask("(999) 999-9999");
+  $('input#card-number').mask("9999 9999 9999 9999");
 });
 
 window.addEventListener('resize', disableHiddenInputs)
